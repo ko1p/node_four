cb2ddebc6d066cb5f8b587bd080bfb0b8a67f928
ko1p <30528165+ko1p@users.noreply.github.com>
2020-06-11 12:32:47 +0300

    add crash-test request

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ mongoose.connect(DATABASE_URL, DATABASE_OPTIONS);
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+      throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
